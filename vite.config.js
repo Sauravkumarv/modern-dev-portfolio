a484a04fb26b158fdb479ce1c78f0c74ff93d2a7
Saurav Kumar Verma <sauravkumarv22@gmail.com>
2026-03-10 09:34:10 +0530

    Mobile Issues Done

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "/modern-dev-portfolio/",
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/modern-dev-portfolio/" : "/",
   plugins: [react()],
-});
+  server: {
+    host: "0.0.0.0",
+  },
+  preview: {
+    host: "0.0.0.0",
+  },
+}));
